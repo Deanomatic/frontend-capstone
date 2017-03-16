64d8569c00e37e4793c8583c5099813d52d3b4d1
Dean Smith <Dean Smith>
2017-03-16 18:17:28 -0500

    IT WORKSgit add .git add .

--- app/factories/mapFactory.js
+++ app/factories/mapFactory.js
@@ -1,1 +1,8 @@
 "use strict";
+
+app.factory("MapFactory", function($q, $http, FBCreds){
+	let saveLocation = function(coord){
+		$http.post(`${FBCreds.databaseURL}/coord.json`, angular.toJson(coord));
+	};
+return {saveLocation};
+})
