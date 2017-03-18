cab8bca5182aa53a49c294569ce35a246bf48c96
Dean Smith <Dean Smith>
2017-03-18 15:25:00 -0500

    Added multiple markers and text box. Not what I want yet.

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -1,5 +1,5 @@
 "use strict";
-
+ 
 app.controller("mapCtrl", function($window, $scope, $timeout, UserdataFactory, AuthFactory) {
 $scope.user = AuthFactory.getUser();
 console.log("Can I get a map up in here??");
@@ -8,9 +8,38 @@ $timeout(function() {
 
     var map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 36.15429, lng: -86.76401},
-      zoom: 14
+      zoom: 14 
     });
-    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    var locations = [];
+    locations.push ({name: "my spot", latlng: new google.maps.LatLng(36.15429, -86.76421)});
+    locations.push ({name: "my spot", latlng: new google.maps.LatLng(36.15729, -86.76432)});
+    locations.push ({name: "my spot", latlng: new google.maps.LatLng(36.16829, -86.76401)});
+
+    for(var i=0; i<locations.length; i++) {
+      var marker = new google.maps.Marker ({
+        position: locations[i].latlng,
+        map: map, 
+        title: locations[i].name
+      });
+    }
+
+    marker =  new google.maps.Marker({
+        position: new google.maps.LatLng(36.15429, -86.76401),
+        map: map,
+        title: "Buck here",
+        content: "Animal and weather and what not"
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+      map: map,
+      content: "Animal, weather and time."
+    });
+
+    google.maps.event.addListener(marker, "click", function() {
+      infoWindow.open(map, marker);
+    });
+
     console.log("map", map);
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -23,7 +52,7 @@ $timeout(function() {
         UserdataFactory.saveLocation(pos);
         console.log("pos", pos);
         infoWindow.setPosition(pos);
-        infoWindow.setContent('Location found.');
+        infoWindow.setContent('Location found. Buck stops here!');
         map.setCenter(pos);
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
@@ -32,7 +61,6 @@ $timeout(function() {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
- 
 
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
