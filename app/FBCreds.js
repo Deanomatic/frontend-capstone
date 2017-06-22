5213971222be4278fb11a9dee3ab56bea30e9f35
Deanomatic <jonthandsmith19@gmail.com>
2017-06-22 15:34:14 -0500

    Adding cdns

---
+++ app/FBCreds.js
@@ -0,0 +1,7 @@
+"use strict";
+
+app.constant("FBCreds", {
+    apiKey: "AIzaSyC38Jm_3t4DqSYnhxnEm4-d5XXos9bRzcU",
+    authDomain: "capstone-65838.firebaseapp.com",
+    databaseURL: "https://capstone-65838.firebaseio.com"
+});
